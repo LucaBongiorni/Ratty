7869e07110a20c8a02aec49861f756000b27d3ce
Sogomn <Sogomn@Sogomn-PC>
2016-02-28 22:32:56 +0100

    Minor changes

--- Ratty/src/de/sogomn/rat/Ratty.java
+++ Ratty/src/de/sogomn/rat/Ratty.java
@@ -2,7 +2,6 @@ package de.sogomn.rat;
 
 import java.io.File;
 import java.net.URI;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.swing.JDialog;
@@ -25,7 +24,7 @@ public final class Ratty {
 	
 	public static final boolean DEBUG = true;
 	public static final String VERSION = "1.10";
-	public static final ResourceBundle LANGUAGE = ResourceBundle.getBundle("language.lang", new Locale("tr"));
+	public static final ResourceBundle LANGUAGE = ResourceBundle.getBundle("language.lang");
 	
 	private static String address;
 	private static int port;
