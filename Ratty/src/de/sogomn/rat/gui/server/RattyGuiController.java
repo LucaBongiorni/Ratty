420dd041025996e9885ee17c82574e5fd4951c93
Sogomn <Sogomn@Sogomn-PC>
2016-03-24 23:18:53 +0100

    Minor changes Reduced client file size

--- Ratty/src/de/sogomn/rat/gui/server/RattyGuiController.java
+++ Ratty/src/de/sogomn/rat/gui/server/RattyGuiController.java
@@ -79,7 +79,8 @@ public final class RattyGuiController extends AbstractRattyController implements
 		"language/lang_uk.properties",
 		"language/lang_pl.properties",
 		"language/lang_dk.properties",
-		"language/lang_it.properties"
+		"language/lang_it.properties",
+		"language/lang_sv.properties"
 	};
 	
 	private static final String FREE_WARNING = LANGUAGE.getString("server.free_warning");
