c197b259f0dfac048b845457e29c24a6d426eab1
Sogomn <Sogomn@Sogomn-PC>
2016-03-24 23:17:07 +0100

    Minor changes Changed version tag

--- Ratty/src/de/sogomn/rat/Ratty.java
+++ Ratty/src/de/sogomn/rat/Ratty.java
@@ -24,7 +24,7 @@ import de.sogomn.rat.server.ActiveServer;
 public final class Ratty {
 	
 	public static final boolean DEBUG = true;
-	public static final String VERSION = "1.20.1";
+	public static final String VERSION = "1.20.2";
 	public static final ResourceBundle LANGUAGE = ResourceBundle.getBundle("language.lang");
 	
 	private static String address;
